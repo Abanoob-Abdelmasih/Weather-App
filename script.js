c36db6449e440f633d05dd174c1a389fa6e3baf7
Abanoob Abdelmasih <abanoob.naji@hotmail.com>
2022-06-09 23:22:20 +0300

    basic weather app

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 "use strict";
-// API Data GET
+// const apiUrlMultipleDays =
+//   "http://api.openweathermap.org/data/2.5/forecast?lat=29.3117&lon=47.4818&units=metric&appid=4c81cb1988ddaba2743cd79b625f27cd";
+
+// axios({
+//   method: "get",
+//   url: apiUrlMultipleDays,
+//   responseType: "stream",
+// }).then((res) => {
+//   console.log(res.data.list[0]);
+// });
+
+const body = document.querySelector("body");
+const weatherImg = document.querySelector("#weather-img");
+const temperature = document.querySelector(".temperature");
+const country = document.querySelector(".country");
+const skyStatus = document.querySelector(".sky-status");
+const humidity = document.querySelector(".humidity");
+const windSpeed = document.querySelector(".wind-speed");
 
 const apiUrlCurrent =
-  "https://api.openweathermap.org/data/2.5/weather?lat=29.3117&lon=47.4818&appid=4c81cb1988ddaba2743cd79b625f27cd";
+  "https://api.openweathermap.org/data/2.5/weather?lat=29.3117&units=metric&lon=47.4818&appid=4c81cb1988ddaba2743cd79b625f27cd";
 
 axios({
   method: "get",
@@ -10,18 +27,17 @@ axios({
   responseType: "stream",
 }).then((res) => {
   console.log(res.data);
+  temperature.innerText = `${Math.round(res.data.main.temp)}°C`;
+  country.innerText = `${res.data.name}, ${res.data.sys.country}`;
+  skyStatus.innerText = `${res.data.weather[0].description}`;
+  humidity.innerText = `${res.data.main.humidity}%`;
+  windSpeed.innerText = `${Math.round(res.data.wind.speed)} m/s`;
+  // //////////////    Icon related  ////////////////////////////////
+  const icon = res.data.weather[0].icon;
+  const iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+  weatherImg.src = iconURL;
+  // //////////////////////////////////////////////////////////////////
 });
 
-// const apiUrlMultipleDays =
-//   "http://api.openweathermap.org/data/2.5/forecast?lat=29.3117&lon=47.481&units=metric&appid=4c81cb1988ddaba2743cd79b625f27cd";
-
-const apiUrlMultipleDays =
-  "http://api.openweathermap.org/data/2.5/forecast?lat=29.3117&lon=47.4818&units=metric&appid=4c81cb1988ddaba2743cd79b625f27cd";
-
-axios({
-  method: "get",
-  url: apiUrlMultipleDays,
-  responseType: "stream",
-}).then((res) => {
-  console.log(res.data);
-});
+// by city name
+// "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
